c8d24cdc72a48737f7394e37ab9b700078457892
Wapeto <dupetopetoman@gmail.com>
2023-12-27 15:41:17 +0100

    Dynamically place cards in the container

--- src/components/ClassCardsContainer.tsx
+++ src/components/ClassCardsContainer.tsx
@@ -1,24 +1,15 @@
-import React from 'react'
-import ClassCard from './ClassCard.tsx'
+import React from "react";
+import ClassCard from "./ClassCard.tsx";
 
-export default function ClassCardsContainer() {
-  const examLists = {
-    "Analyse" : [
-      "CC1",
-      "CC2",
-      "Projet"
-    ]
-  }
+export default function ClassCardsContainer({classList, currentLevel, onChange}) {
+
+
+
   return (
-    <div className='inline-flex max-w-70 justify-center items-start content-center gap-9 flex-wrap mt-32'>
-        <ClassCard classTitle="Analyse" imageLink='analyse.png' examList={examLists["Analyse"]}/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
-        <ClassCard classTitle="POO" imageLink='poo.png'/>
+    <div className="inline-flex max-w-70 justify-center items-start content-center gap-9 flex-wrap mt-28">
+      {Object.keys(classList).map((classTitle, index) => (
+        <ClassCard key={index} classTitle={classTitle} imageLink={classList[classTitle].imageName} examList={classList[classTitle].exams} onInputChange={onChange} currentLevel={currentLevel}/>
+      ))}
     </div>
-  )
+  );
 }
