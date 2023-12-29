f8e9a66245282faceeeba97507896fb45fd1bfa0
Wapeto <dupetopetoman@gmail.com>
2023-12-29 10:23:51 +0100

    Show average when card is filled

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -3,106 +3,107 @@ import ArrowIcon from "./ArrowIcon.tsx";
 
 // Simplified image import and selection logic
 const images = {
-  analyse: ["analyse-1.png", "analyse-2.png"],
-  poo: ["poo-1.png", "poo-2.png"],
-  anglais: ["anglais-1.png", "anglais-2.png"],
-  // ... other classes
+	analyse: ["analyse-1.png", "analyse-2.png"],
+	poo: ["poo-1.png", "poo-2.png"],
+	anglais: ["anglais-1.png", "anglais-2.png"],
+	// ... other classes
 };
 
 const getRandomImage = (className) => {
-  const imageFiles = images[className] || ["default.png"];
-  const randomIndex = Math.floor(Math.random() * imageFiles.length);
-  return require(`../assets/images/${imageFiles[randomIndex]}`);
+	const imageFiles = images[className] || ["default.png"];
+	const randomIndex = Math.floor(Math.random() * imageFiles.length);
+	return require(`../assets/images/${imageFiles[randomIndex]}`);
 };
 
 interface ClassCardProps {
-  classTitle: string;
-  examList?: string[];
-  onInputChange: (id: string, value: string) => void;
-  average?: number;
+	classTitle: string;
+	examList?: string[];
+	onInputChange: (id: string, value: string) => void;
+	_average?: number;
 }
 
-const ClassCard: FC<ClassCardProps> = ({
-  classTitle,
-  examList,
-  onInputChange,
-  average,
-}) => {
-  const cardRef = useRef<HTMLDivElement>(null);
-  const [isFolded, setFolded] = useState(true);
-  const [imageUrl, setImageUrl] = useState("");
+const ClassCard: FC<ClassCardProps> = ({ classTitle, examList, onInputChange, _average }) => {
+	const cardRef = useRef<HTMLDivElement>(null);
+	const [isFolded, setFolded] = useState(true);
+	const [imageUrl, setImageUrl] = useState("");
+	const [average, setAverage] = useState(-1);
 
-  useEffect(() => {
-    setImageUrl(getRandomImage(classTitle.toLowerCase().replace(/\s+/g, "")));
-  }, [classTitle]);
+	useEffect(() => {
+		setImageUrl(getRandomImage(classTitle.toLowerCase().replace(/\s+/g, "")));
+	}, [classTitle]);
 
-  const handleCardUnfold = () => {
-    setFolded(!isFolded);
-    if (isFolded && cardRef.current) {
-      cardRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
-  };
+	useEffect(() => {
+		if (_average !== undefined) {
+			setAverage(_average);
+		}
+	}, [_average]);
 
-  const handleInput = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    exam: string
-  ) => {
-    onInputChange(`${classTitle}-${exam}`, e.target.value);
-  };
+	const handleCardUnfold = () => {
+		setFolded(!isFolded);
+		if (isFolded && cardRef.current) {
+			cardRef.current.scrollIntoView({ behavior: "smooth", block: "center" });
+		}
+	};
 
-  return (
-    <div
-      ref={cardRef}
-      className={`flex flex-col w-48 ${
-        isFolded ? "h-48" : "h-64"
-      } content-center items-center cursor-pointer`}
-      {...(isFolded && { onClick: handleCardUnfold })}
-    >
-      {/* Image */}
-      <div
-        style={{ "--image-url": `url(${imageUrl})` } as React.CSSProperties}
-        className={`class-card-image w-full ${
-          isFolded ? "h-32" : "h-28"
-        } rounded-t-lg border-primary-500 border-x-2 border-t-2 bg-[image:var(--image-url)]`}
-        {...(!isFolded && { onClick: handleCardUnfold })}
-      ></div>
-      {/* Bottom */}
-      <div
-        className={`card-folding-transition w-full overflow-hidden bg-secondary-300 rounded-b-lg border-x-2 border-b-2 border-primary-400 ${
-          isFolded
-            ? "h-[20%] inline-flex items-center justify-center"
-            : "flex flex-col items-center h-full"
-        }`}
-      >
-        <div
-          className={`inline-flex justify-center items-center gap-2 ${
-            !isFolded && "border-b-2 border-text-950"
-          }`}
-          {...(!isFolded && { onClick: handleCardUnfold })}
-        >
-          <p
-            className={`text-center text-text-950 text-lg font-semibold ${
-              !isFolded && "mt-1"
-            }`}
-          >
-            {classTitle}
-          </p>
-          <ArrowIcon direction={isFolded ? "down" : "up"} />
-        </div>
-        <div className={`${isFolded ? "hidden" : "block"} mt-2`}>
-          {examList?.map((exam, index) => (
-            <input
-              key={index}
-              type="text"
-              placeholder={exam[0]}
-              onChange={(e) => handleInput(e, exam[0])}
-              className="w-[60%] my-2 bg-primary-300/50 placeholder-black/50 text-center font-bold border-primary-600 border-2 rounded-lg"
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>, exam: string) => {
+		onInputChange(`${classTitle}-${exam}`, e.target.value);
+	};
+
+	return (
+		<div
+			ref={cardRef}
+			className={`flex flex-col w-48 ${
+				isFolded ? "h-48" : "h-64"
+			} content-center items-center cursor-pointer`}
+			{...(isFolded && { onClick: handleCardUnfold })}>
+			{/* Image */}
+			<div
+				style={{ "--image-url": `url(${imageUrl})` } as React.CSSProperties}
+				className={`inline-flex items-center justify-center class-card-image w-full ${
+					isFolded ? "h-32" : "h-28"
+				} rounded-t-lg border-primary-500 border-x-2 border-t-2 bg-[image:var(--image-url)]`}
+				{...(!isFolded && { onClick: handleCardUnfold })}>
+				<p
+					className={`${
+						average !== -1 ? "opacity-100" : "opacity-0"
+					} average-show-transition text-center text-text-950/75 text-2xl font-semibold bg-primary-200/70 rounded-lg border-2 border-primary-800 px-3 py-1`}>
+					{average !== -1 ? average : ""}
+				</p>
+			</div>
+			{/* Bottom */}
+			<div
+				className={`card-folding-transition w-full overflow-hidden bg-secondary-300 rounded-b-lg border-x-2 border-b-2 border-primary-400 ${
+					isFolded
+						? "h-[20%] inline-flex items-center justify-center"
+						: "flex flex-col items-center h-full"
+				}`}>
+				<div
+					className={`inline-flex justify-center items-center gap-2 ${
+						!isFolded && "border-b-2 border-text-950"
+					}`}
+					{...(!isFolded && { onClick: handleCardUnfold })}>
+					<p
+						className={`text-center text-text-950 text-lg font-semibold ${
+							!isFolded && "mt-1"
+						}`}>
+						{classTitle}
+					</p>
+					<ArrowIcon direction={isFolded ? "down" : "up"} />
+				</div>
+				<div className={`${isFolded ? "hidden" : "block"} mt-2`}>
+					{examList?.map((exam, index) => (
+						<input
+							key={index}
+							type="text"
+							placeholder={exam[0]}
+							onChange={(e) => handleInput(e, exam[0])}
+							className="w-[60%] my-2 bg-primary-300/50 placeholder-black/50 text-center font-bold border-primary-600 border-2 rounded-lg"
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ClassCard;
--- src/components/ClassCardsContainer.tsx
+++ src/components/ClassCardsContainer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ClassCard from "./ClassCard.tsx";
 
-export default function ClassCardsContainer({classList, currentLevel, onChange}) {
-
-
-
-  return (
-    <div className="inline-flex max-w-70 justify-center items-start content-center gap-9 flex-wrap mt-28">
-      {Object.keys(classList).map((classTitle, index) => (
-        <ClassCard key={index} classTitle={classTitle} examList={classList[classTitle].exams} onInputChange={onChange}/>
-      ))}
-    </div>
-  );
+export default function ClassCardsContainer({ classList, currentLevel, onChange, averages }) {
+	return (
+		<div className="inline-flex max-w-70 justify-center items-start content-center gap-9 flex-wrap mt-28">
+			{Object.keys(classList).map((classTitle, index) => (
+				<ClassCard
+					key={index}
+					classTitle={classTitle}
+					examList={classList[classTitle].exams}
+					onInputChange={onChange}
+					_average={averages[`${currentLevel}-${classTitle}`] || -1}
+				/>
+			))}
+		</div>
+	);
 }
