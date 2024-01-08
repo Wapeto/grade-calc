16489b54c0dc437ea4ffda7569128b299548e7ba
Wapeto <dupetopetoman@gmail.com>
2024-01-08 14:17:53 +0100

    Solve error with parseFloat (now works with strings and thus accepts values like "10.5")

--- src/components/Exam.tsx
+++ src/components/Exam.tsx
@@ -4,15 +4,16 @@ const Exam = ({ examName, examGradeCoef, onExamUpdate }) => {
     const isCalculatedGrade = examGradeCoef[0] === -1;
 
     const handleExamValueChange = (e) => {
-        const value = parseFloat(e.target.value);
-        onExamUpdate(examName, isNaN(value) ? -1 : value);
+        // const value = parseFloat(e.target.value);
+        const value = e.target.value;
+        onExamUpdate(examName, value === "" ? -1 : value);
     }
 
     return (
         <input
             type="text"
             placeholder={examName}
-            value={examGradeCoef[0] !== -1 ? examGradeCoef[0] : ""}
+            value={isCalculatedGrade ? "" : examGradeCoef[0]}
             onChange={(e) =>handleExamValueChange(e)}
             className={`w-[60%] my-2 bg-primary-300/50 placeholder-black/50 text-center font-bold border-primary-600 border-2 rounded-lg ${isCalculatedGrade ? "calculated-grade-class" : "user-grade-class"}`}
         />
