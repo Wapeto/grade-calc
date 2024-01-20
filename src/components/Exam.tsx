ec8821d7c77257a08e922f3fe16005afbdea1cca
Wapeto <dupetopetoman@gmail.com>
2024-01-20 16:01:06 +0100

    Refactor grade input field in Exam component

--- src/components/Exam.tsx
+++ src/components/Exam.tsx
@@ -17,7 +17,7 @@ const Exam = ({ examName, examGradeCoef, onExamUpdate, updateEditedState }) => {
 			<label htmlFor={examName} className="text-base font-normal">
 				{examName}
 			</label>
-			<div className="grade-input inline-flex items-center">
+			<div className="input-field inline-flex items-center">
 				<input
 					type="number"
 					name={examName}
@@ -25,6 +25,7 @@ const Exam = ({ examName, examGradeCoef, onExamUpdate, updateEditedState }) => {
 					className="w-24 h-8 border-y border-l border-secondary-200 rounded-l-md p-2 text-sm"
 					// placeholder={isCalculatedGrade ? "N/A" : examGradeCoef[0]}
 					onChange={handleExamValueChange}
+					value={isCalculatedGrade ? "" : examGradeCoef[0]}
 				/>
 				<div className="h-full text-base font-normal text-text-500 bg-white border-y border-r border-secondary-200  rounded-r px-2 flex items-center">{`Coef ${examGradeCoef[1]}`}</div>
 			</div>
