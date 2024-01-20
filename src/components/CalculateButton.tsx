5db2a5b8287ef5e2c0a8097866053e700545f7c2
Wapeto <dupetopetoman@gmail.com>
2024-01-20 16:00:24 +0100

    Restyle calculate button

--- src/components/CalculateButton.tsx
+++ src/components/CalculateButton.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function CalculateButton({onClick}) {
   return (
-    <button className='my-12 py-2 px-4 bg-primary-500/60 rounded-lg border-secondary-500 border-2 font-semibold' onClick={onClick}>
-        Calculate
+    <button className='py-2 px-4 h-8 flex items-center bg-primary-500 rounded-lg font-semibold text-white' onClick={onClick}>
+        Calculer
     </button>
   )
 }
