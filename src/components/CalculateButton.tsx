118c1036f062f896192cf0181f215d7e5d1199e5
Wapeto <dupetopetoman@gmail.com>
2024-01-22 19:22:49 +0100

    Change button height

--- src/components/CalculateButton.tsx
+++ src/components/CalculateButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CalculateButton({onClick}) {
   return (
-    <button className='py-2 px-4 h-8 flex items-center bg-primary-500 rounded-lg font-semibold text-white' onClick={onClick}>
+    <button className='py-2 px-4 h-10 flex items-center bg-primary-500 rounded-lg font-semibold text-white' onClick={onClick}>
         Calculer
     </button>
   )
--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ResetButton({onClick}) {
   return (
-    <button className='py-2 px-4 h-8 flex items-center border border-secondary-200 rounded-lg font-semibold text-text-950' onClick={onClick}>
+    <button className='py-2 px-4 h-10 flex items-center border border-secondary-200 rounded-lg font-semibold text-text-950' onClick={onClick}>
         Réinitialiser
     </button>
   )
